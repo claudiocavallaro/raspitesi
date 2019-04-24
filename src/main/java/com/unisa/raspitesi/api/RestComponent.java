c0182b2d44377a8f344e49916edd9cd21fd2d51b
lords91 <claudio.cavallaro91@gmail.com>
2019-04-24 18:29:51 +0200

    ...

--- src/main/java/com/unisa/raspitesi/api/RestComponent.java
+++ src/main/java/com/unisa/raspitesi/api/RestComponent.java
@@ -1,6 +1,10 @@
 package com.unisa.raspitesi.api;
 
 import com.mashape.unirest.http.Unirest;
+import com.unisa.raspitesi.Application;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,30 +12,10 @@ public class RestComponent {
 
 
 
-    private String sendGet(String getUrl) {
+    @RequestMapping(value = "/restart", method = RequestMethod.GET)
+    public void reStart(){
 
-        String noresult = "no results from sendGet on " + getUrl + " - check logs";
-
-        int numTry = 5;
-
-        while(numTry > 0) {
-
-            try{
-                com.mashape.unirest.http.HttpResponse<String> R = Unirest.get(getUrl)
-                        .header("Content-Type", "application/json")
-                        .header("Cache-Control", "no-cache")
-                        .asString();
-                return R.getBody().toString();
-
-            } catch (Exception e) {
-
-                numTry --;
-                if(numTry == 0) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-        return noresult;
+        Application app = new Application();
     }
+
 }
