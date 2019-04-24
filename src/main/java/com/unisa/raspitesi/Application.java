a88f5a372a9fada0302e028d90d58067e938e7ac
lords91 <claudio.cavallaro91@gmail.com>
2019-04-24 10:39:04 +0200

    ...

--- src/main/java/com/unisa/raspitesi/Application.java
+++ src/main/java/com/unisa/raspitesi/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @EnableSwagger2
 @SpringBootApplication
 public class Application {
@@ -18,17 +21,36 @@ public class Application {
 
 
     public Application(){
-        sendGet("http://localhost:8080/api/entrance");
+
+        while (true) {
+            ProcessBuilder pb = new ProcessBuilder("python","readNoT.py");
+            try {
+                Process p = pb.start();
+
+                BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line = "";
+                line = bfr.readLine();
+
+                while ((line = bfr.readLine())!= null) {
+                    System.out.println(line);
+                }
+
+                if (!line.equals("No card")){
+                    break;
+                }
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        sendGet("http://192.168.1.92.:8080/api/entrance");
     }
 
     private String sendGet(String getUrl) {
-
         String noresult = "no results from sendGet on " + getUrl + " - check logs";
-
         int numTry = 5;
-
         while(numTry > 0) {
-
             try{
                 com.mashape.unirest.http.HttpResponse<String> R = Unirest.get(getUrl)
                         .header("Content-Type", "application/json")
@@ -38,15 +60,12 @@ public class Application {
                 return R.getBody().toString();
 
             } catch (Exception e) {
-
                 numTry --;
                 if(numTry == 0) {
                     e.printStackTrace();
                 }
-
             }
         }
         return noresult;
     }
-
 }
