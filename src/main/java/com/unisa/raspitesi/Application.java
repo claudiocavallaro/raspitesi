139d1f3b804ffa721685f4012cbf8eb7f8a84b4d
lords91 <claudio.cavallaro91@gmail.com>
2019-04-24 11:49:20 +0200

    ...

--- src/main/java/com/unisa/raspitesi/Application.java
+++ src/main/java/com/unisa/raspitesi/Application.java
@@ -22,13 +22,13 @@ public class Application {
 
     public Application(){
 
-/*        while (true) {
+        String line = "";
+        while (true) {
             ProcessBuilder pb = new ProcessBuilder("python","readNoT.py");
             try {
                 Process p = pb.start();
 
                 BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String line = "";
                 line = bfr.readLine();
 
                 while ((line = bfr.readLine())!= null) {
@@ -42,12 +42,12 @@ public class Application {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }*/
+        }
 
-        sendGet("http://192.168.1.92:8080/api/entrance");
+        sendGet("http://192.168.1.92:8080/api/entrance", line);
     }
 
-    private String sendGet(String getUrl) {
+    private String sendGet(String getUrl, String line) {
         String noresult = "no results from sendGet on " + getUrl + " - check logs";
         int numTry = 5;
         while(numTry > 0) {
@@ -55,7 +55,7 @@ public class Application {
                 com.mashape.unirest.http.HttpResponse<String> R = Unirest.get(getUrl)
                         .header("Content-Type", "application/json")
                         .header("Cache-Control", "no-cache")
-                        .queryString("uid", "uid1")
+                        .queryString("uid", line)
                         .asString();
                 return R.getBody().toString();
 
