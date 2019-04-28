eaeaa06c664cf4724683527a2aed706262c816d5
lords91 <claudio.cavallaro91@gmail.com>
2019-04-28 10:48:23 +0200

    ...

--- src/main/java/com/unisa/raspitesi/api/RestComponent.java
+++ src/main/java/com/unisa/raspitesi/api/RestComponent.java
@@ -2,13 +2,17 @@ package com.unisa.raspitesi.api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.pi4j.io.gpio.*;
 import com.unisa.raspitesi.model.Read;
 import com.unisa.raspitesi.model.ReadEvent;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestComponent {
+
+    private static GpioPinDigitalOutput pin;
 
 
     @GetMapping("/api/restart")
@@ -19,6 +23,20 @@ public class RestComponent {
 
         return result;
 
+    }
+
+
+    @RequestMapping("/api/confirm")
+    public String getConfirm(){
+
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "LED", PinState.LOW);
+        }
+
+        pin.toggle();
+
+        return "OK";
     }
 
 }
