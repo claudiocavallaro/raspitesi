1a98aea4f179b2cd78079e308c534c3a0857c695
lords91 <claudio.cavallaro91@gmail.com>
2019-04-29 18:21:48 +0200

    ...

--- src/main/java/com/unisa/raspitesi/api/EventHandler.java
+++ src/main/java/com/unisa/raspitesi/api/EventHandler.java
@@ -32,8 +32,6 @@ public class EventHandler {
         try{
             if (result.equals("Nothing")){
                 System.out.println("No user to display");
-            } else if (result.equals("Non ci sono ingressi disponibili")){
-                System.out.println("Non ci sono ingressi disponibili");
             } else {
                 User user = mapper.readValue(result, User.class);
                 System.out.println(user.toString());
