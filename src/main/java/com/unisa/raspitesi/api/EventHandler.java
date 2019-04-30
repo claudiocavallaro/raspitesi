c50da54056faf9231bca297f5cc85faf749bd9ae
lords91 <claudio.cavallaro91@gmail.com>
2019-04-30 10:37:41 +0200

    ...

--- src/main/java/com/unisa/raspitesi/api/EventHandler.java
+++ src/main/java/com/unisa/raspitesi/api/EventHandler.java
@@ -37,26 +37,31 @@ public class EventHandler {
         Read lastExit = null;
 
         if (recordList.isEmpty()){
+            System.out.println(recordList.keySet().toString());
             recordList.put(event.getRead().getUid(), event.getRead().getTimestamp());
             user = completeSend(event);
         } else {
             if (recordList.containsKey(event.getRead().getUid())){
-                System.out.println("Valore già registrato " + recordList.get(event.getRead().getUid()));
+                System.out.println(recordList.keySet().toString());
+                //System.out.println("Valore già registrato " + recordList.get(event.getRead().getUid()));
 
                 long timeArrive = recordList.get(event.getRead().getUid());
                 long recordValue = event.getRead().getTimestamp();
-                System.out.println("Valore di confronto " + event.getRead().getTimestamp());
+                //System.out.println("Valore di confronto " + event.getRead().getTimestamp());
 
                 if (Math.abs(timeArrive - recordValue) < 6000){
                     System.out.println("no get to send");
                 } else {
-
                     user = completeSend(event);
                     recordList.remove(event.getRead().getUid());
                     lastExit = new Read(event.getRead().getUid());
                     lastExit.setTimestamp(event.getRead().getTimestamp());
+
+                    System.out.println(recordList.keySet().toString());
+                    
                 }
             } else {
+
                 System.out.println("LAST EXIT " + lastExit.toString());
                 if (event.getRead().getUid().equals(lastExit.getUid()) && Math.abs(event.getRead().getTimestamp() - lastExit.getTimestamp()) < 6000){
                     System.out.println("Utente appena uscito");
