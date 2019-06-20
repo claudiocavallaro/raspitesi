cf42aee9ad0fa0cb40631e63a7da5231c5f43d2d
lords91 <claudio.cavallaro91@gmail.com>
2019-06-20 14:07:03 +0200

    ...

--- src/main/java/com/unisa/raspitesi/api/EventHandler.java
+++ src/main/java/com/unisa/raspitesi/api/EventHandler.java
@@ -154,7 +154,7 @@ public class EventHandler {
 
     public User completeSend(ReadEvent event) {
         System.out.println(event.getRead() + "----- SENDING GET ------");
-        String result = sendGet("http://192.168.1.56:8080/api/entrance", event.getRead().getUid());
+        String result = sendGet("http://progettotesi.ddns.net:33425/api/entrance", event.getRead().getUid());
         User user = null;
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -208,7 +208,7 @@ public class EventHandler {
         // Qui posso anche fare in modo che se la palestra è vuota non manda alcuna get, giusto per risparmiare risorse
 
         System.out.println("----- SENDING GET ------");
-        String result = sendGetPower("http://192.168.1.56:8080/api/power", powerEvent.getPower());
+        String result = sendGetPower("http://progettotesi.ddns.net:33425/api/power", powerEvent.getPower());
     }
 
     private String sendGetPower(String s, Power power) {
@@ -250,7 +250,7 @@ public class EventHandler {
         System.out.println(camera.toString());
 
         System.out.println("----- SENDING GET ------");
-        String result = sendGetCamera("http://192.168.1.56:8080/api/camera", cameraEvent.getCamera());
+        String result = sendGetCamera("http://progettotesi.ddns.net:33425/api/camera", cameraEvent.getCamera());
     }
 
     private String sendGetCamera(String s, Camera camera) {
--- src/main/java/com/unisa/raspitesi/api/ReadComponent.java
+++ src/main/java/com/unisa/raspitesi/api/ReadComponent.java
@@ -10,7 +10,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-@Component
+//@Component
 public class ReadComponent implements DisposableBean, Runnable {
 
     private Thread thread;
